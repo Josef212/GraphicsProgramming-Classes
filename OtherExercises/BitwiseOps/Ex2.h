c62bbab1007cac98590326a4008f0028bee0678c
Josef21296 <jcasanovasv@hotmail.com>
2018-02-15 19:25:45 +0100

    more fails

--- OtherExercises/BitwiseOps/Ex2.h
+++ OtherExercises/BitwiseOps/Ex2.h
@@ -9,9 +9,9 @@ int isContained(byte b, ushort u)
 	// TO DO: Insert your code here
 	ushort _b = b;
 
-	for (int i = 0; i < 16; ++i)
+	for (int i = 0; i < 8; ++i)
 	{
-		if ((_b << i) & u) return 1;
+		if ((_b << i) & u == (_b << i)) return 1;
 	}
 
 	return 0;
